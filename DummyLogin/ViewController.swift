9bb07b6c17e3055f12cc47c65e3dd13abb33640e
arkan481 <arkanharyo@gmail.com>
2020-06-17 13:11:31 +0700

    Initial Commit

---
+++ DummyLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DummyLogin
+//
+//  Created by Arkan Haryo on 6/17/20.
+//  Copyright © 2020 Arkan Haryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
